ea6bc3b7be9aa996892b93e99c2388a04a80b5be
Pravin <40529452+dadasur@users.noreply.github.com>
2026-06-12 23:33:22 +0530

    Assignment2

---
+++ PythonCode/Assignment2/ArithmeticOperations.py
@@ -0,0 +1,18 @@
+#6. Write a program that accepts two numbers #from user and prints their:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+
+No1 = int(input("Enter first number: "))
+No2 = int(input("Enter second number: "))
+
+
+addition = No1 + No2
+subtraction = No1 - No2
+multiplication = No1 * No2
+
+
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
---
+++ PythonCode/Assignment2/PythonBuiltInFunction.py
@@ -0,0 +1,11 @@
+#1.Write a program to display:
+#Value
+#Type
+#Memory address
+#for a variable using appropriate built-in functions.
+
+
+no=10
+print("Value : ", (no))
+print("Type : ", type(no))
+print("Memory Address :", id(no))
