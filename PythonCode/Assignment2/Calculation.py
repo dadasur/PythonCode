cb8655393f3b3260d1acd77da87e46fb5d951d34
Pravin <40529452+dadasur@users.noreply.github.com>
2026-06-12 23:06:32 +0530

    Assignment2

---
+++ PythonCode/Assignment2/Calculation.py
@@ -0,0 +1,13 @@
+# Accept two numbers from the user
+No1 = int(input("Enter first number: "))
+No2 = int(input("Enter second number: "))
+
+# Perform operations
+addition = No1 + No2
+subtraction = No1 - No2
+multiplication = No1 * No2
+
+# Display results
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
